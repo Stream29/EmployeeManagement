56a6f5e9692bfd7ef6b34566a81b7d69c64438f8
Stream <1542763342@qq.com>
2024-07-11 06:33:24 +0800

    feat: Employee.java

--- src/main/java/com/stream/employeemanagement/pojo/Employee.java
+++ src/main/java/com/stream/employeemanagement/pojo/Employee.java
@@ -1,5 +1,18 @@
 package com.stream.employeemanagement.pojo;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@Builder
 public class Employee
 {
+    private int id;
+    private String name;
+    private String sex;
+    private String phone;
+    private Instant createTime;
+    private Instant updateTime;
 }
